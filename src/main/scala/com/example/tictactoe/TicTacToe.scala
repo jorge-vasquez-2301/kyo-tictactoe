e324679b7d916fa09aa8aa46453d184e9d127995
Jorge Vasquez <jorge.vasquez0123@proton.me>
2023-12-19 10:59:03 -0400

    Fix

--- src/main/scala/com/example/tictactoe/TicTacToe.scala
+++ src/main/scala/com/example/tictactoe/TicTacToe.scala
@@ -6,6 +6,7 @@ import com.example.tictactoe.view.confirm.ConfirmViewLive
 import kyo.*
 import kyo.direct.*
 import kyo.envs.*
+import kyo.layers.*
 import kyo.ios.*
 import kyo.options.*
 import com.example.tictactoe.terminal.Terminal
@@ -21,48 +22,46 @@ import com.example.tictactoe.gameLogic.GameLogicLive
 import com.example.tictactoe.opponentAi.OpponentAiLive
 import com.example.tictactoe.controller.ControllerLive
 import com.example.tictactoe.terminal.TerminalLive
-import kyo.layers.Layer
 import com.example.tictactoe.mode.confirm.ConfirmMode
 import com.example.tictactoe.mode.menu.MenuMode
 import com.example.tictactoe.mode.game.GameMode
 
 object TicTacToe extends KyoApp:
+  val program: State > (Envs[Terminal] & Envs[Controller] & IOs) =
+    def step(state: State): State > (Envs[Terminal] & Envs[Controller] & Options & IOs) =
+      defer {
+        val terminal   = await(Envs[Terminal].get)
+        val controller = await(Envs[Controller].get)
+        await(terminal.display(controller.render(state)))
+        val input      = if state == State.Shutdown then "" else await(terminal.getUserInput)
+        await(controller.process(input, state))
+      }
+
+    def loop(state: State): State > (Envs[Terminal] & Envs[Controller] & IOs) =
+      IOs {
+        Options.run[State, Envs[Terminal] & Envs[Controller] & IOs](step(state)).map {
+          case Some(nextState) => loop(nextState)
+          case None            => state
+        }
+      }
+
+    loop(State.initial)
+
+  val layer: Layer[Envs[Controller] & Envs[Terminal], Any] =
+    val confirmMode: Layer[Envs[ConfirmMode], Any] =
+      ConfirmModeLive.layer chain (ConfirmCommandParserLive.layer andThen ConfirmViewLive.layer)
+
+    val menuMode: Layer[Envs[MenuMode], Any] =
+      MenuModeLive.layer chain (MenuCommandParserLive.layer andThen MenuViewLive.layer)
+
+    val gameMode: Layer[Envs[GameMode], Any] =
+      GameModeLive.layer chain (GameCommandParserLive.layer andThen GameViewLive.layer andThen GameLogicLive.layer andThen OpponentAiLive.layer)
+
+    val controller: Layer[Envs[Controller], Any] =
+      ControllerLive.layer chain (confirmMode andThen menuMode andThen gameMode)
+
+    controller andThen TerminalLive.layer
 
   run {
-    val program: State > (Envs[Terminal] & Envs[Controller] & IOs) =
-      def step(state: State): State > (Envs[Terminal] & Envs[Controller] & Options & IOs) =
-        defer {
-          val terminal   = await(Envs[Terminal].get)
-          val controller = await(Envs[Controller].get)
-          await(terminal.display(controller.render(state)))
-          val input      = if state == State.Shutdown then "" else await(terminal.getUserInput)
-          await(controller.process(input, state))
-        }
-
-      def loop(state: State): State > (Envs[Terminal] & Envs[Controller] & IOs) =
-        IOs {
-          Options.run[State, Envs[Terminal] & Envs[Controller] & IOs](step(state)).map {
-            case Some(nextState) => loop(nextState)
-            case None            => state
-          }
-        }
-
-      loop(State.initial)
-
-    val layer: Layer[Envs[Controller] & Envs[Terminal], Any] =
-      val confirmMode: Layer[Envs[ConfirmMode], Any] =
-        ConfirmModeLive.layer chain (ConfirmCommandParserLive.layer andThen ConfirmViewLive.layer)
-
-      val menuMode: Layer[Envs[MenuMode], Any] =
-        MenuModeLive.layer chain (MenuCommandParserLive.layer andThen MenuViewLive.layer)
-
-      val gameMode: Layer[Envs[GameMode], Any] =
-        GameModeLive.layer chain (GameCommandParserLive.layer andThen GameViewLive.layer andThen GameLogicLive.layer andThen OpponentAiLive.layer)
-
-      val controller: Layer[Envs[Controller], Any] =
-        ControllerLive.layer chain (confirmMode andThen menuMode andThen gameMode)
-
-      controller andThen TerminalLive.layer
-
     layer.run(program)
   }
