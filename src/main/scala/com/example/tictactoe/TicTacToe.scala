4529fef9a5258a4111acc2fab191b80a75336447
Jorge Vasquez <jorge.vasquez0123@proton.me>
2023-12-19 14:59:51 -0400

    Refactor

--- src/main/scala/com/example/tictactoe/TicTacToe.scala
+++ src/main/scala/com/example/tictactoe/TicTacToe.scala
@@ -25,21 +25,32 @@ import com.example.tictactoe.terminal.TerminalLive
 import com.example.tictactoe.mode.confirm.ConfirmMode
 import com.example.tictactoe.mode.menu.MenuMode
 import com.example.tictactoe.mode.game.GameMode
+import com.example.tictactoe.view.confirm.ConfirmView
+import com.example.tictactoe.parser.confirm.ConfirmCommandParser
+import com.example.tictactoe.parser.menu.MenuCommandParser
+import com.example.tictactoe.view.menu.MenuView
+import com.example.tictactoe.view.game.GameView
+import com.example.tictactoe.opponentAi.OpponentAi
+import com.example.tictactoe.gameLogic.GameLogic
+import com.example.tictactoe.parser.game.GameCommandParser
 
 object TicTacToe extends KyoApp:
   val program: State > (Envs[Terminal] & Envs[Controller] & IOs) =
-    def step(state: State): State > (Envs[Terminal] & Envs[Controller] & Options & IOs) =
-      defer {
-        val terminal   = await(Envs[Terminal].get)
-        val controller = await(Envs[Controller].get)
-        await(terminal.display(controller.render(state)))
-        val input      = if state == State.Shutdown then "" else await(terminal.getUserInput)
-        await(controller.process(input, state))
-      }
+    def step(state: State): Option[State] > (Envs[Terminal] & Envs[Controller] & IOs) =
+      val nextState: State > (Envs[Terminal] & Envs[Controller] & IOs & Options) =
+        defer {
+          val terminal   = await(Envs[Terminal].get)
+          val controller = await(Envs[Controller].get)
+          await(terminal.display(controller.render(state)))
+          val input      = if state == State.Shutdown then "" else await(terminal.getUserInput)
+          await(controller.process(input, state))
+        }
+
+      Options.run(nextState)
 
     def loop(state: State): State > (Envs[Terminal] & Envs[Controller] & IOs) =
       IOs {
-        Options.run[State, Envs[Terminal] & Envs[Controller] & IOs](step(state)).map {
+        step(state).map {
           case Some(nextState) => loop(nextState)
           case None            => state
         }
@@ -48,19 +59,28 @@ object TicTacToe extends KyoApp:
     loop(State.initial)
 
   val layer: Layer[Envs[Controller] & Envs[Terminal], Any] =
-    val confirmMode: Layer[Envs[ConfirmMode], Any] =
-      ConfirmModeLive.layer chain (ConfirmCommandParserLive.layer andThen ConfirmViewLive.layer)
+    val confirmModeDeps: Layer[Envs[ConfirmCommandParser] & Envs[ConfirmView], Any] =
+      ConfirmCommandParserLive.layer andThen ConfirmViewLive.layer
 
-    val menuMode: Layer[Envs[MenuMode], Any] =
-      MenuModeLive.layer chain (MenuCommandParserLive.layer andThen MenuViewLive.layer)
+    val menuModeDeps: Layer[Envs[MenuCommandParser] & Envs[MenuView], Any] =
+      MenuCommandParserLive.layer andThen MenuViewLive.layer
 
-    val gameMode: Layer[Envs[GameMode], Any] =
-      GameModeLive.layer chain (GameCommandParserLive.layer andThen GameViewLive.layer andThen GameLogicLive.layer andThen OpponentAiLive.layer)
+    val gameModeDeps: Layer[Envs[GameCommandParser] & Envs[GameView] & Envs[GameLogic] & Envs[OpponentAi], Any] =
+      GameCommandParserLive.layer andThen GameViewLive.layer andThen GameLogicLive.layer andThen OpponentAiLive.layer
 
-    val controller: Layer[Envs[Controller], Any] =
-      ControllerLive.layer chain (confirmMode andThen menuMode andThen gameMode)
+    val confirmModeNoDeps: Layer[Envs[ConfirmMode], Any] =
+      ConfirmModeLive.layer chain confirmModeDeps
 
-    controller andThen TerminalLive.layer
+    val menuModeNoDeps: Layer[Envs[MenuMode], Any] =
+      MenuModeLive.layer chain menuModeDeps
+
+    val gameModeNoDeps: Layer[Envs[GameMode], Any] =
+      GameModeLive.layer chain gameModeDeps
+
+    val controllerNoDeps: Layer[Envs[Controller], Any] =
+      ControllerLive.layer chain (confirmModeNoDeps andThen menuModeNoDeps andThen gameModeNoDeps)
+
+    controllerNoDeps andThen TerminalLive.layer
 
   run {
     layer.run(program)
